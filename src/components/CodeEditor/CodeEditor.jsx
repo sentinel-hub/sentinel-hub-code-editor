8921715cc79f5596712fb9c6e7c21a0473e82e2e
Pontus Berg <pontusberg95@gmail.com>
2021-12-15 08:43:03 +0100

    revert add jshint import as named

--- src/components/CodeEditor/CodeEditor.jsx
+++ src/components/CodeEditor/CodeEditor.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef } from "react";
 import { ArrowsExpandIcon, XIcon } from "@heroicons/react/solid";
 import loader from "@monaco-editor/loader";
-import JSHINT from "jshint";
+import {JSHINT} from "jshint";
 import useFreeEditor from "../../hooks/useFreeEditor";
 import { IoIosResize } from "react-icons/io";
 import { BiFullscreen, BiExitFullscreen, BiExpand } from "react-icons/bi";
@@ -95,7 +95,7 @@ export const CodeEditor = ({ onRunEvalscriptClick }) => {
 
   const checkAndApplyErrors = debounce(function () {
     const code = editorRef.current.getValue();
-    JSHINT.JSHINT(code, JSHINT_CONFIG);
+    JSHINT(code, JSHINT_CONFIG);
     const errors = JSHINT.errors.map((e) => {
       return {
         startLineNumber: e.line,
