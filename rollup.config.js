a21746c81d293b268803607eb98ae605b58af12d
Pontus Berg <pontusberg95@gmail.com>
2022-01-25 16:21:33 +0100

    remove rollup postcss plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
-import path from 'path'
 import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import commonjs from '@rollup/plugin-commonjs'
 
