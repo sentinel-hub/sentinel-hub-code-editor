be5ee05c0ea5d83f546676116d76e978163a9279
pberg <pontus.berg@sinergise.com>
2022-02-16 22:40:39 +0100

    add insert evalscript manually as utility function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {CodeEditor} from './components/CodeEditor/CodeEditor'
 import {themeEdcBrowserDark, themeEdcBrowserLight} from './components/CodeEditor/editor-themes/themesEdcBrowser'
 import {useCodeEditorUtils} from './hooks/useCodeEditorUtils'
+import {manuallyInsertEvalscriptIntoEditor} from './utils/manuallyInsertEvalscriptIntoEditor'
 
-export {CodeEditor, themeEdcBrowserLight, themeEdcBrowserDark, useCodeEditorUtils}
+export {CodeEditor, themeEdcBrowserLight, themeEdcBrowserDark, useCodeEditorUtils, manuallyInsertEvalscriptIntoEditor}
