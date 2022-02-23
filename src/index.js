624f4d4141da706a2140350feff2b988be4c2b0c
pberg <pontus.berg@sinergise.com>
2022-02-23 11:21:47 +0100

    remove dist folder from repo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import {CodeEditor} from './components/CodeEditor/CodeEditor'
 import {themeEdcBrowserDark, themeEdcBrowserLight} from './components/CodeEditor/editor-themes/themesEdcBrowser'
-import {useCodeEditorUtils} from './hooks/useCodeEditorUtils'
 import {manuallyInsertEvalscriptIntoEditor} from './utils/manuallyInsertEvalscriptIntoEditor'
 
-export {CodeEditor, themeEdcBrowserLight, themeEdcBrowserDark, useCodeEditorUtils, manuallyInsertEvalscriptIntoEditor}
+export {CodeEditor, themeEdcBrowserLight, themeEdcBrowserDark, manuallyInsertEvalscriptIntoEditor}
