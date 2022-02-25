91a856cc41d6fc4aa4ba451bb6cbd30e0daaf01f
pberg <pontus.berg@sinergise.com>
2022-02-25 11:22:35 +0100

    fit export typo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import {CodeEditor} from './components/CodeEditor/CodeEditor'
 import {themeEdcBrowserDark, themeEdcBrowserLight} from './components/CodeEditor/editor-themes/themesEdcBrowser'
 import {themeEoBrowserDark, themeEoBrowserLight} from './components/CodeEditor/editor-themes/themesEoBrowser'
-export {CodeEditor, themeEoBrowserDark, themeEdcBrowserDark, themeEdcBrowserLight, themeEdcBrowserDark}
+export {CodeEditor, themeEoBrowserDark, themeEoBrowserLight, themeEdcBrowserLight, themeEdcBrowserDark}
