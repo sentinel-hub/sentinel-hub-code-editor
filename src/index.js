5e804597df233043aef7d1e6f7daea0bde854ad5
pberg <pontus.berg@sinergise.com>
2022-02-25 11:14:21 +0100

    add new themes to export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import {CodeEditor} from './components/CodeEditor/CodeEditor'
 import {themeEdcBrowserDark, themeEdcBrowserLight} from './components/CodeEditor/editor-themes/themesEdcBrowser'
-
-export {CodeEditor, themeEdcBrowserLight, themeEdcBrowserDark}
+import {themeEoBrowserDark, themeEoBrowserLight} from './components/CodeEditor/editor-themes/themesEoBrowser'
+export {CodeEditor, themeEoBrowserDark, themeEdcBrowserDark, themeEdcBrowserLight, themeEdcBrowserDark}
