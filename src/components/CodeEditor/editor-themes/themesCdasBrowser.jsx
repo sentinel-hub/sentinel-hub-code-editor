5b87d8bce01b6b1015fa9067829f3191db9957d8
Stelios E Priftis <stelios.priftis@sinergise.com>
2023-01-13 17:04:42 +0100

    Provide Cdas overlay value color

--- src/components/CodeEditor/editor-themes/themesCdasBrowser.jsx
+++ src/components/CodeEditor/editor-themes/themesCdasBrowser.jsx
@@ -1,6 +1,7 @@
 export const themeCdasBrowserDark = {
   name: "cdas-browser-dark",
   styles: {
+    colorOverlayBg: "rgba(0,0,0, 0.3)",
     colorUI500: "#f4f4f4",
     colorBg400: "#3a3d3d",
     colorBg500: "#3a3d3d",
@@ -15,6 +16,7 @@ export const themeCdasBrowserDark = {
 export const themeCdasBrowserLight = {
   name: "cdas-browser-light",
   styles: {
+    colorOverlayBg: "rgba(205, 205, 215, 0.5)",
     colorBg500: "#ffffff",
     colorBg600: "#f4f4f4",
     colorUI500: "#363636",
